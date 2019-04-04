5bdc9d0ce306c74aaf711076017ba1ce963cb935
Ercan Serteli <xercax@gmail.com>
2019-04-04 14:22:41 +0300

    Net2Net wider for MLPs started

--- source.jl
+++ source.jl
@@ -76,6 +76,40 @@ function random_pad_mlp(model, changing_layer, new_unit_count)
     return new_model
 end
 
+function wider_mlp(model, changing_layer, new_unit_count)
+    old_unit_count = size(model.layers[changing_layer].w, 1)
+    if old_unit_count >= new_unit_count
+        println("New unit count must be greater than old unit count")
+        return nothing
+    end
+
+    new_model = deepcopy(model)
+    layer = new_model.layers[changing_layer]
+
+    # Initialize number of extra units, extra w array, random mapping, number of copies
+    extra_count = new_unit_count - old_unit_count
+    extra_w = Array{Float32}(undef, (extra_count, size(layer.w, 2)))
+    extra_mapping = rand(1:old_unit_count, extra_count)
+    copy_counts = ones(old_unit_count)
+    for i in 1:extra_count
+        copy_counts[extra_mapping[i]] += 1
+    end
+    # Divide weights that are copied more than once
+    layer.w ./= copy_counts
+
+    # Set the extra weights
+    for i in 1:extra_count
+        extra_w[i] = layer.w[extra_mapping[i]]
+    end
+
+
+    layer.w = Param(vcat(layer.w, extra_w))
+    layer.b = Param(vcat(layer.b, padding_b))
+    new_model.layers[changing_layer+1].w = Param(hcat(new_model.layers[changing_layer+1].w, padding_2))
+
+    return new_model
+end
+
 # The global device setting (to reduce gpu() calls)
 let at = nothing
     global atype
